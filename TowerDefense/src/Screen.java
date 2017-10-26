e2c26e94c1c2f2f9b35e3df7b9429ffb7dd671ee
IkerPS <32924001+IkerPS@users.noreply.github.com>
2017-10-26 18:00:22 +0200

    Clase screen de la interfaz

---
+++ TowerDefense/src/Screen.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Screen extends JPanel{
+		
+	public Screen(){
+		
+	}
+	
+	public void paintComponent(Graphics g){
+		
+	}
+}
