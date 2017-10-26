acf22ca65ee0663d2445eeb93cc697af3762614d
IkerPS <32924001+IkerPS@users.noreply.github.com>
2017-10-26 19:11:43 +0200

    Cuadrícula del juego

--- TowerDefense/src/Screen.java
+++ TowerDefense/src/Screen.java
@@ -3,13 +3,39 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-public class Screen extends JPanel{
-		
+public class Screen extends JPanel implements Runnable{
+	
+	public Thread thread = new Thread(this);
+	public static int myWidth, myHeight;
+	public static boolean isFirst = true;
+	
+	public static Room room;
 	public Screen(){
-		
+		thread.start();
+	}
+	
+	public void define(){
+		room = new Room();
 	}
 	
 	public void paintComponent(Graphics g){
+		if(isFirst){
+			define();
+			isFirst= false;
+			
+		}
+		g.clearRect(0, 0, getWidth(), getHeight());
 		
+		room.draw(g); // dibujando room
+		
+	}
+	public void run(){
+		while(true){
+			if(!isFirst){
+				room.physic();
+				
+			}
+			repaint();
+		}
 	}
 }
